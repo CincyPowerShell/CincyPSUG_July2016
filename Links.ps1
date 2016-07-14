3c8a04877544f3c0e255727eb647c06f7f84759c
Matt McNabb <mmcnabb@outlook.com>
2016-07-14 15:02:23 -0400

    resolve conflict

--- Links.ps1
+++ Links.ps1
@@ -1,38 +1,3 @@
-<<<<<<< Updated upstream
-# official PowerShell for Office 365 site
-Start-Process "http://powershell.office.com/"
-
-# Office 365 for IT Pros
-Start-Process "https://www.amazon.com/Office-365-Pros-Tony-Redmond-ebook/dp/B01G2LLVAI/ref=sr_1_1?ie=UTF8&qid=1468417433&sr=8-1&keywords=office+365+for+it+pros"
-
-# download dependencies
-# Skype and Sharepoint don't install to the module path!
-Start-Process "https://technet.microsoft.com/en-us/library/dn568015.aspx"
-
-# Azure AD module preview with ADAL
-Start-Process "https://connect.microsoft.com/site1164/content/content.aspx?ContentID=32016"
-
-# new Azure AD module preview
-Start-Process "https://azure.microsoft.com/en-us/updates/azure-ad-new-powershell-cmdlets-preview/" 
-
-# my blog
-Start-Process "http://mattmcnabb.github.io"
-
-# my blog
-Start-Process "https://github.com/mattmcnabb"
-
-# O365 Admin Center (not mine)
-Start-Process "https://github.com/bwya77/O365-Admin-Center/"
-
-# O365 Service Health module
-Start-Process "https://github.com/mattmcnabb/O365ServiceCommunications"
-
-# Office 365 APIs
-Start-Process "https://msdn.microsoft.com/en-us/office/office365/howto/rest-api-overview"
-
-# Microsoft Graph API
-Start-Process "http://graph.microsoft.io/en-us/"
-=======
 # official PowerShell for Office 365 site
 Start-Process "http://powershell.office.com/"
 
@@ -63,4 +28,3 @@ Start-Process "https://msdn.microsoft.com/en-us/office/office365/howto/rest-api-
 
 # Microsoft Graph API
 Start-Process "http://graph.microsoft.io/en-us/"
->>>>>>> Stashed changes
